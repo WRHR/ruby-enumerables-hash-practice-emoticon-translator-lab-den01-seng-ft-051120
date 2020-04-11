f42795031db2bf745bd2e9a3648f1f2f4f08c745
Will Reeves <wrhreeves@gmail.com>
2020-04-11 19:37:32 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -11,10 +11,12 @@ def load_library(file_path)
   library
 end
 
-def get_japanese_emoticon
+def get_japanese_emoticon(file_path, emoticon)
   # code goes here
 end
 
-def get_english_meaning
+def get_english_meaning(file_path, emoticon)
   # code goes here
+  load_library(file_path)[:japanese][emoticon]
+  
 end
